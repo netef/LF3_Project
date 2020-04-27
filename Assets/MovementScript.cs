099742f0fce3e880df0e0b57a1f02d2d9f48dfbd
netef <netef.work@gmail.com>
2020-04-27 20:38:54 +0300

    first upload

---
+++ Assets/MovementScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    Rigidbody2D rb;
+    float inputX = 0;
+    float inputY = 0;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        inputX = Input.GetAxisRaw("Horizontal");
+        inputY = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(inputX * Time.fixedDeltaTime * 150, inputY * Time.fixedDeltaTime * 150);
+    }
+}
